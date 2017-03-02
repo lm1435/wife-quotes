f5912af7f41be0875b2f72b67ad9a386559f9178
Luis Meza <luismeza01@gmail.com>
2017-03-02 17:10:35 -0600

    reduced js code

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ var imageArray = ["images/1.jpg","images/2.jpg","images/3.jpg",
   "images/4.jpg","images/5.jpg","images/6.jpg", "images/7.jpg", "images/8.jpg"];
 var colors = ['#67D5B5', '#EE7785', '#C89EC4', '#84B1ED', '#D4DFE6', '#9DC3C1', '#FFEEE4', '#dedcee'];
 var i = 0;
-var j = 0;
-var h = 0;
 var imageDiv = document.getElementById('images');
 imageDiv.classList.add("img-responsive");
 imageDiv.style.margin = "auto";
@@ -13,10 +11,10 @@ document.getElementById('display').style.fontSize = "32px";
 var body = document.body;
 
 document.getElementById("button").addEventListener('click', function(){
-  body.style.backgroundColor = colors[h];
-  h = (h+1) % colors.length;
-  imageDiv.setAttribute("src", imageArray[j]);
-  j = (j+1) % imageArray.length;
+  body.style.backgroundColor = colors[i];
+
+  imageDiv.setAttribute("src", imageArray[i]);
+
   var display = "";
   display += arr[i];
   i = (i+1) % arr.length;
